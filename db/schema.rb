a68d12cdd079549d2b4ea16263c04398e0c76ddf
Till Richter <robintill81@gmail.com>
2020-05-21 23:42:14 +0200

    Prepare rails app with external specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_122340) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cocktails", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "doses", force: :cascade do |t|
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "cocktail_id", null: false
-    t.bigint "ingredient_id", null: false
-    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
-    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
-  end
-
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "doses", "cocktails"
-  add_foreign_key "doses", "ingredients"
 end
